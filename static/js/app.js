0809eabf15f400fc0298f5c1dbc1964c5a3dd988
odmishien <tmtm.yagi.007@gmail.com>
2019-05-23 01:41:13 +0900

    add tweet buttton

--- static/js/app.js
+++ static/js/app.js
@@ -19,11 +19,16 @@ $(function() {
       total_with_tax = res["total_with_tax"];
 
       $('#result-products').empty();
+      $('#twitter-share-area').empty();
+      var products_text_for_twitter = '';
+
       for(let product of products) {
         $('#result-products').append(`<tr><td>${product.ProductName}</td><td>${product.Price}</td><td>${product.PriceWithTax}</td></tr>`);
+        products_text_for_twitter = products_text_for_twitter + product.ProductName + '\n'
       }
       $('#result-total').html('¥' + total);
       $('#result-total-with-tax').html('¥' + total_with_tax);
+      $('#twitter-share-area').append(`<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="セブンプレミアムガチャを回したよ \n ${products_text_for_twitter}みんなも回してね\n" data-url="https://seven-premium-gacha.herokuapp.com/" data-lang="ja" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`);
     }).fail(function(jqXHR, textStatus,errorThrown) {
       alert("サーバとの通信に失敗しました...");
     });
