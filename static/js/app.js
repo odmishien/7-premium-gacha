59863b78a68595748ec263ee785edc89be06c5b0
odmishien <tmtm.yagi.007@gmail.com>
2019-05-22 22:09:58 +0900

    launch ajax

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,32 @@
 $(function() {
   $('#gacha-button').on('click',function(e) {
-    let form = $('#gacha-form').get()[0];
-    let formData = new FormData(form);
-    console.log(formData);
+    let total = $('#gacha-form #total').val();
+    let genre = $('input[name="genre[]"]:checked').map(function(){ return $(this).val(); }).get();
+    var json = {
+      total: total,
+      genre: genre
+    };
 
     $.ajax({
       url: '/gacha',
       method: 'post',
       dataType: 'json',
-      data: formData,
-      processData: false,
-      contentType: false
+      contentType: 'application/json',
+      data: JSON.stringify(json),
     }).done(function(res) {
-      console.log(res);
-    }).fail(function(jqXHR, textStatus,errorThrown) {
-      console.log('ERROR', jqXHR, textStatus, errorThrown);
-    });
+      products = res["products"];
+      total = res["total"];
+      total_with_tax = res["total_with_tax"];
 
+      $('#result-products').empty();
+      for(let product of products) {
+        $('#result-products').append(`<tr><td>${product.ProductName}</td><td>${product.Price}</td><td>${product.PriceWithTax}</td></tr>`);
+      }
+      $('#result-total').html('¥' + total);
+      $('#result-total-with-tax').html('¥' + total_with_tax);
+    }).fail(function(jqXHR, textStatus,errorThrown) {
+      alert("サーバとの通信に失敗しました...");
+    });
     return false;
   });
 });
