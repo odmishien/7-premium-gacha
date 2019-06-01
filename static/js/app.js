09178c573e9dd3ba7fa1006cf8ad2ade02aadc23
odmishien <tmtm.yagi.007@gmail.com>
2019-06-01 17:25:11 +0900

    tweet update

--- static/js/app.js
+++ static/js/app.js
@@ -24,8 +24,10 @@ $(function() {
 
       for(let product of products) {
         $('#result-products').append(`<tr><td>${product.ProductName}</td><td>${product.Price}</td><td>${product.PriceWithTax}</td></tr>`);
-        products_text_for_twitter = products_text_for_twitter + product.ProductName + '\n'
+        products_text_for_twitter = products_text_for_twitter + product.ProductName + ' ' +  product.PriceWithTax + '円\n'
       }
+
+      products_text_for_twitter = products_text_for_twitter + '合計' + total_with_tax + '円'
       $('#result-total').html('¥' + total);
       $('#result-total-with-tax').html('¥' + total_with_tax);
       $('#twitter-share-area').append(`<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="セブンプレミアムガチャを回したよ\n${products_text_for_twitter}みんなも回してね\n" data-url="https://seven-premium-gacha.herokuapp.com/" data-lang="ja" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`);
